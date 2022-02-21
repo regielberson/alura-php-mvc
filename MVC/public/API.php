8b2c9917bdc2e962ac92818a49e3c61e6feea215
Regielberson Alexandre Pereira Lirio <rlirio@supriservice.local>
2022-02-21 16:16:56 -0300

    teste da Ana

--- MVC/public/API.php
+++ MVC/public/API.php
@@ -6,7 +6,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $client = new \GuzzleHttp\Client();
 
-
+//test test 
 
 
 $response = $client->request('POST', 'http://192.168.100.240:8380/mge/service.sbr?serviceName=MobileLoginSP.login&outputType=json');
